2dcaf7a48fa6c4a240b80f39dd4d4081660f4737
SergioAnd18 <developinc@yandex.ru>
2017-03-29 13:09:05 +0300

    - remove app menu

--- menu/templatetags/menu_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-from menu.models import MenuItem
-register = template.Library()
-
-
-@register.inclusion_tag('menu/menu.html')
-def draw_menu():
-    return {'menu': MenuItem.objects.all()}
